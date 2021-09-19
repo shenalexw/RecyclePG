37e0efd88dd7c7c3238b082c0bde1eeac5cb75a5
Alex Shen <shenalexw@gmail.com>
2021-09-19 14:42:03 -0700

    final push

--- game.js
+++ game.js
@@ -1,17 +1,23 @@
-let logged = 1;
-let health = 50000;// Change this one to update based on database health
+if (localStorage.getItem("boss") == null){
+    localStorage.setItem("boss", 50000)
+}
+
+let health = localStorage.getItem("boss");// Change this one to update based on database health
 let startstring = "-------- I will flood the streets with rage! --------"
 
 // Update the life bar of both the boss and the database
 function updateLife(life){
     if (life < 0){
         health = 0
+        localStorage.removeItem("boss");
+        localStorage.setItem("boss", 0)
     } else {
+        localStorage.removeItem("boss");
+        localStorage.setItem("boss", life)
         health = life; // Update the database health as well.
     }
     document.getElementById('num_health').innerHTML
         = health;
-    
 }
 
 // Update the source of the image based on the given health.
@@ -25,10 +31,10 @@ function updateCloud(life){
 }
 
 // Updates the message underneath the cloud.
-    function updateMessage(sentence){
-        let element =  document.getElementById("display_message")
-        element.innerHTML = sentence;
-    }
+function updateMessage(sentence){
+    let element =  document.getElementById("display_message")
+    element.innerHTML = sentence;
+}
 
 // Attack the boss
 document.getElementById("attack_button").addEventListener("click", function(event){
@@ -42,15 +48,21 @@ document.getElementById("attack_button").addEventListener("click", function(even
     let paper = parseInt(document.getElementById('paper').value);
     let ewaste = parseInt(document.getElementById('ewaste').value);
 
+
+    // If the boss is defeated,display happy message
+    if (health == 0){
+        message = "------- Thank you saving me! ------- ";
+    }
+
     // If any of the levels are > 5, then a 2x multiplyer is added
-    if (cans > 5 || bottles > 5 || reuse > 5 || paper > 5 || ewaste > 5){
+    else if (cans > 5 || bottles > 5 || reuse > 5 || paper > 5 || ewaste > 5){
         multiplyer += 5;
         message = "----- Wow, what a powerful move that was! -----";
     }
 
     // If all of the levels are > 5, then a 5x multiplyer is added
     else if (cans > 5 && bottles > 5 && reuse > 5 && paper > 5 && ewaste > 5){
-        multiplyer += 10;
+        multiplyer += 50000;
         message = "-------- This can't Be!!!!!!!!!!!!!!!!!!!!! --------";
     }
 
@@ -63,8 +75,8 @@ document.getElementById("attack_button").addEventListener("click", function(even
     else {
         message = "------------- You call that an attack! -------------";
     }
-    
-    // Add up all the recycling 
+
+    // Add up all the recycling
     let total = cans + bottles + reuse + paper + ewaste;
     let final = total * multiplyer;
     let temp_health = health - final;
@@ -74,21 +86,14 @@ document.getElementById("attack_button").addEventListener("click", function(even
     updateCloud(temp_health);
     updateMessage(message);
 
-    // If Cans and bottles are 
     event.preventDefault();
 });
 
+document.getElementById("reset_health").addEventListener("click", function(event){
+    localStorage.removeItem("boss");
+    localStorage.setItem("boss", 50000);
+});
 
-// Determines if the log in button is shown or the log out button is shown.
-function logDisplay(check) {
-    if (check == 0){
-        document.getElementById("in").style.display = "none";
-    }
-    else {
-        document.getElementById("out").style.display = "none";
-
-    }
-}
 
 function myFunction() {
     var x = document.getElementById("myLinks");
@@ -99,6 +104,5 @@ function myFunction() {
     }
 }
 
-logDisplay(logged);
 updateLife(health);
 updateCloud(health);
